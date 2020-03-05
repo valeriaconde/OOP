b01fc9a4a27af8d3f67aa2114c58b1c925ff0af7
Valeria Conde <valeriaconde.96@gmail.com>
2020-03-05 13:23:02 -0600

    Added filesinput files to final project

---
+++ OOPproyectoIntegrador/OOPproyectoIntegrador/main.cpp
@@ -0,0 +1,52 @@
+//
+//  main.cpp
+//  OOPproyectoIntegrador
+//
+//  Created by Valeria Conde
+//  A01281637
+//  05/03/20.
+//  Copyright © 2020 Valeria Conde. All rights reserved.
+//
+
+/*
+ 
+ Tu programa deberá de resolver las siguientes preguntas que deberán de formar parte de un menú de opciones
+
+ 1. Hora de cada uno de los días con mayor saturación.
+ 2. Cantidad de llegadas por hora promedio.
+ 3. Cantidad de salidas por hora promedio.
+
+ Ejemplo Pregunta 2 y 3 (Los datos no son de acuerdo al archivo)
+ hora 0 - 20 salidas y 30 llegadas promedio
+ hora 1 - 35 salidas y 50 llegadas promedio
+ hora 2 - 40 salidas y 33 llegadas promedio
+ así para todas las horas.
+
+ 4. Cantidad de llegadas y salidas por aerolínea.
+ 5. Cantidad de pasajeros atendidos por día.
+ 6. Porcentaje promedio de capacidad utilizada en los aviones, es decir la cantidad de pasajeros entre la capacidad de pasajeros de todos los vuelos cada día.
+ 7. Pregunta que creas que ya faltado para complementar el análisis de la situación problema
+
+ El proyecto debe de contener por lo menos 3 clases y al menos una clase con composición.
+ 
+ 
+ datos vuelos:
+ Formato: (Fecha(YYYY/MM/DD) Hora(HH:MM) Vuelo Llegada(A)/Salida(S) Aerolínea Destino Avión Pasajeros Capacidad)
+ 
+ aviones
+ Formato: (Avion Pasajeros)
+ 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+
+
+
+    return 0;
+}
